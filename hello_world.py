878689c48aa9759ccef449e9c4163dc6ce9133e8
Roberto Leon-Luna <bertleon@umich.edu>
2019-09-09 12:47:51 -0400

    first commit

---
+++ hello_world.py
@@ -0,0 +1,17 @@
+import random
+
+def main():
+	print('Roberto')
+	num1 = random.randint(0,100)
+	num2 = random.randint(0,100)
+	sums = num1 + num2
+	avg = sums/2
+	print(num1)
+	print(num2) 
+	print("Sum =  ", sums)
+	print("Average = ", avg)
+
+   
+
+if __name__ == "__main__":
+    main()
